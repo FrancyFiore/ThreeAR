3c4ba18ffcd24281699d4163fe5e3e93da6be2a7
Francesco Fiorelli <francesco@MBPdiFrancesco.homenet.telecomitalia.it>
2020-12-14 10:34:05 +0100

    add model

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function initialize()
 
     let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.5 );
     scene.add( ambientLight );
-                
+
     camera = new THREE.Camera();
     scene.add(camera);
 
@@ -38,10 +38,10 @@ function initialize()
 	light.position.set( 0, 10, 0 ); // default; light shining from top
 	light.castShadow = true;
     scene.add( light );
-    
+
     // Animation clock
     clock = new THREE.Clock();
-    
+
     // setup arToolkitSource
     arToolkitSource = new THREEx.ArToolkitSource({
         sourceType : 'webcam',
@@ -49,30 +49,30 @@ function initialize()
 
     function onResize()
     {
-        arToolkitSource.onResize()	
-        arToolkitSource.copySizeTo(renderer.domElement)	
+        arToolkitSource.onResize()
+        arToolkitSource.copySizeTo(renderer.domElement)
         if ( arToolkitContext.arController !== null )
         {
-            arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)	
-        }	
+            arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)
+        }
     }
 
     arToolkitSource.init(function onReady(){
         onResize()
     });
-    
+
     // handle resize event
     window.addEventListener('resize', function(){
         onResize()
     });
-    
-   
+
+
     // create atToolkitContext
     arToolkitContext = new THREEx.ArToolkitContext({
         cameraParametersUrl: 'js/lib/data/camera_para.dat',
         detectionMode: 'mono'
     });
-    
+
     // copy projection matrix to camera when initialization complete
     arToolkitContext.init( function onCompleted(){
         camera.projectionMatrix.copy( arToolkitContext.getProjectionMatrix() );
@@ -84,44 +84,44 @@ function initialize()
     let markerControls = new THREEx.ArMarkerControls(arToolkitContext, markerRoot, {
         type: 'pattern', patternUrl: "js/lib/data/hiro.patt",
     })
-    
-    // const loader = new THREE.GLTFLoader();
-    // loader.crossOrigin = true;
-    // Load the car model
-    // loader.load( 'models/urus/Urus.gltf', function ( gltf ) {
-    //     let model = gltf.scene;
-    //     model.position.z = -10; 
-    //     model.rotation.x = Math.PI / 2;
 
-    //     markerRoot.add(model);
-    //     Create an AnimationMixer, and get the list of AnimationClip instances
-    //     mixer = new THREE.AnimationMixer( gltf.scene );
-    //     animations = gltf.animations;
+    const loader = new THREE.GLTFLoader();
+    loader.crossOrigin = true;
+    Load the car model
+    loader.load( 'models/urus/Urus.gltf', function ( gltf ) {
+        let model = gltf.scene;
+        model.position.z = -10;
+        model.rotation.x = Math.PI / 2;
 
-    //     for(let i = 0; i < 4; i++){
+        markerRoot.add(model);
+        Create an AnimationMixer, and get the list of AnimationClip instances
+        mixer = new THREE.AnimationMixer( gltf.scene );
+        animations = gltf.animations;
 
-    //         mixer.clipAction(animations[i*4]).play();
-    //         mixer.clipAction(animations[i*4]).setLoop( THREE.LoopRepeat, 2 );
-    //         mixer.clipAction(animations[i*4]).clampWhenFinished = true;
+        for(let i = 0; i < 4; i++){
 
-    //         mixer.clipAction(animations[i*4 + 1]).play();
-    //         mixer.clipAction(animations[i*4 + 1]).setLoop( THREE.LoopOnce );
-    //         mixer.clipAction(animations[i*4 + 1]).clampWhenFinished = true;
+            mixer.clipAction(animations[i*4]).play();
+            mixer.clipAction(animations[i*4]).setLoop( THREE.LoopRepeat, 2 );
+            mixer.clipAction(animations[i*4]).clampWhenFinished = true;
 
-    //         mixer.clipAction(animations[i*4 + 2]).play();
-    //         mixer.clipAction(animations[i*4 + 2]).setLoop( THREE.LoopOnce );
-    //         mixer.clipAction(animations[i*4 + 2]).clampWhenFinished = true;
+            mixer.clipAction(animations[i*4 + 1]).play();
+            mixer.clipAction(animations[i*4 + 1]).setLoop( THREE.LoopOnce );
+            mixer.clipAction(animations[i*4 + 1]).clampWhenFinished = true;
 
-    //         mixer.clipAction(animations[i*4 + 3]).play();
-    //         mixer.clipAction(animations[i*4 + 3]).setLoop( THREE.LoopOnce );
-    //         mixer.clipAction(animations[i*4 + 3]).clampWhenFinished = true;
+            mixer.clipAction(animations[i*4 + 2]).play();
+            mixer.clipAction(animations[i*4 + 2]).setLoop( THREE.LoopOnce );
+            mixer.clipAction(animations[i*4 + 2]).clampWhenFinished = true;
 
-    //     }
-    // });
-      
-    
-    // window.addEventListener('click', e => raycast(e, false));
-    // window.addEventListener('touchend', e => raycast(e, true))
+            mixer.clipAction(animations[i*4 + 3]).play();
+            mixer.clipAction(animations[i*4 + 3]).setLoop( THREE.LoopOnce );
+            mixer.clipAction(animations[i*4 + 3]).clampWhenFinished = true;
+
+        }
+    });
+
+
+    window.addEventListener('click', e => raycast(e, false));
+    window.addEventListener('touchend', e => raycast(e, true))
 }
 
 
@@ -135,14 +135,14 @@ function raycast(e, touch = false, controls = null) {
         mouse.x = 2 * (e.clientX / canvasSize.width) - 1;
         mouse.y = 1 - 2 * (e.clientY / canvasSize.height);
     }
-    
+
     raycaster.setFromCamera(mouse, camera);
     const intersects = raycaster.intersectObjects( scene.children, true );
-    
+
     if ( intersects.length > 0 ) {
 
         if(intersects[0].object.parent.parent.name == "FL" || intersects[0].object.parent.parent.name == "FR" ){
-            
+
             // Play wheel animations
             for(let i = 0; i < 4; i++){
 
@@ -196,6 +196,6 @@ function animate()
     //     // Update the animation mixer, the stats panel, and render this frame
     //     mixer.update( mixerUpdateDelta );
     // }
-     
+
     render();
 }
